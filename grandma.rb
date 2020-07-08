359925118a16cc2e13d72a219cb1a11e9404c18e
ariel murphy <arielladders@gmail.com>
2020-07-08 21:35:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elseif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  else phrase
+  else phrase == phrase.upcase
+    return ""
   end
 end
